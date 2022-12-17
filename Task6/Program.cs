84da5e44a20f0fd0ca4b7d73f2f993392a53c0ea
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-17 21:32:00 +0700

    add Task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+System.Console.WriteLine("Задача 6");
+System.Console.Write("Введите число ");// Вывод приглашения ввода.
+string inputValue = Console.ReadLine(); // Ввод стороки с терминала
+int Number = Convert.ToInt32(inputValue);// Приобразование стороки в число
+
+if (Number % 2 == 0)
+{
+    System.Console.Write("Число " + Number + " является чётным.");
+}
+else
+{
+    System.Console.Write("Число " + Number + " является нечётным.");
+}
+
